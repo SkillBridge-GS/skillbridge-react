330ba5ffd8d348fdf38ccc1a05ee9a8aa4f36b87
diogo2506 <diogoabrao@gmail.com>
2025-11-19 17:17:50 -0300

    feat: Configurando useForm para Login e layout inicial da página

--- skillbridge/src/routes/Login/index.tsx
+++ skillbridge/src/routes/Login/index.tsx
@@ -18,3 +18,34 @@ const MOCK_LOGIN_CREDENCIAIS = {
 };
 
 export default function Login() {
+  const navigate = useNavigate();
+
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { errors, isSubmitting } 
+  } = useForm<ILoginData>({
+    mode: "onSubmit",
+    criteriaMode: "firstError",
+    shouldFocusError: true,
+  });
+
+  const onSubmit = () => {}; 
+
+  return (
+    <FormContainer>
+      <h2 className="text-3xl font-bold text-center text-blue-600 dark:text-blue-400">
+        Acesse sua conta
+      </h2>
+      <p className="text-center text-gray-600 dark:text-gray-400">
+        Use as credenciais de teste para entrar: **admin@gmail.com** e **admin**
+      </p>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4" noValidate> 
+        
+
+      </form>
+
+    </FormContainer>
+  );
+}

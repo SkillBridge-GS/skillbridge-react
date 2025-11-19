f459d60686dd2ffec6fa4d24f2265ccc7080f573
LeonardoZerbinattiDeSales <leonardozerbinattidesales@gmail.com>
2025-11-19 15:34:13 -0300

    feat: Implementação da estrutura da navbar e hook.

--- skillbridge/src/components/Menu/Menu.tsx
+++ skillbridge/src/components/Menu/Menu.tsx
@@ -3,10 +3,14 @@ import { useTheme } from "../../../contexts/ThemeContext";
 import { ThemeToggleButton } from "../../ThemeBotao/ThemeBotao";
 
 export default function Menu() {
-  const { theme } = useTheme();
+  const { theme } = useTheme(); 
 
   return (
     <nav className="fixed top-0 z-50 w-full backdrop-blur-sm bg-white/50 dark:bg-gray-900/90 shadow-md">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+        </div>
+      </div>
     </nav>
   );
 }

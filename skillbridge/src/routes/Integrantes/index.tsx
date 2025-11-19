f5976c45c8a4c1e4d06b4800ec68362c07df5b01
lucastdag <lucastav000@gmail.com>
2025-11-19 16:41:51 -0300

    feat: Rota para integrantes, dados do time.

---
+++ skillbridge/src/routes/Integrantes/index.tsx
@@ -0,0 +1,33 @@
+import IntegranteCard from "../../components/IntegranteCard/IntegranteCard"; 
+import LucasFoto from '../../assets/lucas.jpeg'; 
+import LeoFoto from '../../assets/leonardo.jpeg';
+import DiogoFoto from '../../assets/diogo.jpeg';
+
+// Dados do time
+const integrantesData = [
+  {
+    nome: "Lucas Tavares Dagrosa",
+    rm: "RM563424",
+    turma: "1TDSPG",
+    githubUrl: "https://github.com/lucastdag",
+    linkedinUrl: "https://www.linkedin.com/in/lucastdagrosa/",
+    fotoUrl: LucasFoto
+  },
+  {
+    nome: "Leonardo Zerbinatti",
+    rm: "RM67890",
+    turma: "1TDSPH",
+    githubUrl: "https://github.com/LeonardoZerbinattiDeSales",
+    linkedinUrl: "https://www.linkedin.com/in/leonardo-zerbinatti-83a43b307/",
+    fotoUrl: LeoFoto 
+  },
+  {
+    nome: "Diogo Cunha",
+    rm: "RM563654",
+    turma: "1TDSPH",
+    githubUrl: "https://github.com/diogo2506",
+    linkedinUrl: "#",
+    fotoUrl: DiogoFoto 
+  },
+];
+
